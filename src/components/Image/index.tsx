5bbda259f6c32e9b2e8ceb52ad54d9e487f96d93
Luc Perkins <lucperkins@gmail.com>
2022-01-19 12:02:47 -0800

    Add image width to props

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -6,16 +6,18 @@ type Props = {
   src: string;
   caption?: string;
   title?: string; // Browsers display this as a tooltip on hover
+  width?: number;
 };
 
-const Image = ({ alt, src, caption, title }: Props) => {
+const Image = ({ alt, src, caption, title, width }: Props) => {
   const url: string = useBaseUrl(src);
   const imgTitle: string = title || caption;
+  const imgWidth: number = width || 100;
 
   return (
     <figure>
       <a href={url} target="_blank">
-        <img alt={alt} src={url} title={imgTitle} />
+        <img alt={alt} src={url} title={imgTitle} width={`${imgWidth}%`} />
       </a>
 
       {caption && (
