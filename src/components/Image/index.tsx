7d7ebf0f42a63a529978bc42808c56cd7db9c9e9
Luc Perkins <lucperkins@gmail.com>
2022-01-19 09:48:24 -0800

    Add image title to props

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -5,15 +5,17 @@ type Props = {
   alt: string;
   src: string;
   caption?: string;
+  title?: string; // Browsers display this as a tooltip on hover
 };
 
-const Image = ({ alt, src, caption }: Props) => {
+const Image = ({ alt, src, caption, title }: Props) => {
   const url: string = useBaseUrl(src);
+  const imgTitle: string = title || caption;
 
   return (
     <figure>
       <a href={url} target="_blank">
-        <img alt={alt} src={url} />
+        <img alt={alt} src={url} title={imgTitle} />
       </a>
 
       {caption && (
